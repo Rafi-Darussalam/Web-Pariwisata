f002e540a79599a8584ca1080efda6a05ed77841
Rafi darussalam <Rafidarussalam80@gmail.com>
2025-04-04 22:56:45 +0700

    Delete All For Unknown Reason

--- scripty.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    const checkbox = document.getElementById('check');
-  
-    if (window.scrollY > 50) {
-      checkbox.checked = false;
-    }
-  });
