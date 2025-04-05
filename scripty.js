3a5adabe3c0a85cbe8eb8d72af2dd16189ffe8c6
Rafi darussalam <Rafidarussalam80@gmail.com>
2025-04-05 19:08:21 +0700

    Fixed

--- scripty.js
+++ scripty.js
@@ -5,14 +5,3 @@ window.addEventListener('scroll', function() {
       checkbox.checked = false;
     }
   });
-
-  // Klik otomatis setiap detik
-setInterval(() => {
-  document.getElementById("page").click();
-}, 1000);
-
-// Aksi saat tombol diklik
-document.getElementById("page").addEventListener("click", () => {
-  console.log("Tombol #page diklik otomatis.");
-  // Tambah aksi di sini
-});
